d996b6bce7772999fe11ada7d026b3fb586f899e
Vivek Jadhav <117569148+V-Vivek@users.noreply.github.com>
2023-03-12 16:03:16 +0530

    Update reducer.py

--- Hadoop/Hadoop Practical/reducer.py
+++ Hadoop/Hadoop Practical/reducer.py
@@ -1,41 +1,28 @@
-# from operator import itemgetter
+#!/usr/bin/env python
+
 import sys
-  
+
 current_word = None
 current_count = 0
-word = None
-  
-# read the entire line from STDIN
+
+# Input: Word and its count
 for line in sys.stdin:
+    # Strip whitespace and split by tab delimiter
+    word, count = line.strip().split('\t')
 
-    # remove leading and trailing whitespace
-    line = line.strip()
+    # Convert count to int
+    count = int(count)
 
-    # slpiting the data on the basis of tab we have provided in mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-  
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
+    # If the word is the same as the previous word, increment count
+    if word == current_word:
         current_count += count
-
     else:
+        # If the word is different, emit the previous word and its count
         if current_word:
-            # write result to STDOUT
-            print(current_word, '\t', current_count)
-
-        current_count = count
+            print("%s\t%s" % (current_word, current_count))
         current_word = word
-  
-# do not forget to output the last word if needed!
-if current_word == word:
-    print(current_word, '\t', current_count)
+        current_count = count
+
+# Emit the last word and its count
+if current_word:
+    print("%s\t%s" % (current_word, current_count))
